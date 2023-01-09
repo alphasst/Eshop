a2eda6f8d2023d60b9b168f32de1e14e59323f9f
alphasst <george.chirila@365.univ-ovidius.ro>
2023-01-09 11:27:18 +0800

    CREATE NEW PAGE

---
+++ application/views/delivery_boy/pages/forms/tables/cash-collection.php
@@ -0,0 +1,25 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <!-- Main content -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h4>Cash Collection Transactions </h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= base_url('delivery_boy/home') ?>">Home</a></li>
+                        <li class="breadcrumb-item active">Cash Collection Transactions</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
