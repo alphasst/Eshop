938563064ea4742333b7a647193a9448ce586f0e
alphasst <george.chirila@365.univ-ovidius.ro>
2023-03-20 00:35:46 +0800

    CREATE NEW PAGE

---
+++ application/views/admin/pages/forms/tables/manage-orders.php
@@ -0,0 +1,80 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <!-- Main content -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h4>Manage Orders</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= base_url('admin/home') ?>">Home</a></li>
+                        <li class="breadcrumb-item active">Orders</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="transaction_modal" data-backdrop="static" data-keyboard="false">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="user_name">Order Tracking</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="card card-info">
+                                            <!-- form start -->
+                                            <form class="form-horizontal " id="order_tracking_form" action="<?= base_url('admin/orders/update-order-tracking/'); ?>" method="POST" enctype="multipart/form-data">
+                                                <input type="hidden" name="order_id" id="order_id">
+                                                <div class="card-body pad">
+                                                    <div class="form-group ">
+                                                        <label for="courier_agency">Courier Agency</label>
+                                                        <input type="text" class="form-control" name="courier_agency" id="courier_agency" placeholder="Courier Agency" />
+                                                    </div>
+                                                    <div class="form-group ">
+                                                        <label for="tracking_id">Tracking Id</label>
+                                                        <input type="text" class="form-control" name="tracking_id" id="tracking_id" placeholder="Tracking Id" />
+                                                    </div>
+                                                    <div class="form-group ">
+                                                        <label for="url">URL</label>
+                                                        <input type="text" class="form-control" name="url" id="url" placeholder="URL" />
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button type="reset" class="btn btn-warning">Reset</button>
+                                                        <button type="submit" class="btn btn-success" id="submit_btn">Save</button>
+                                                    </div>
+                                                </div>
+                                                <div class="d-flex justify-content-center">
+                                                    <div class="form-group" id="error_box">
+                                                    </div>
+                                                </div>
+                                                <!-- /.card-body -->
+                                            </form>
+                                        </div>
+                                        <!--/.card-->
+                                    </div>
+                                    <!--/.col-md-12-->
+                                </div>
+                                <!-- /.row -->
+
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
